0fe3499578e161d5c280992b5ea6485991d67d1c
Alexander Popiak <alexander.popiak@parity.io>
2020-11-26 14:12:33 +0100

    simplify getApi

--- src/utils/getApi.ts
+++ src/utils/getApi.ts
@@ -2,7 +2,5 @@ import { ApiPromise, WsProvider } from '@polkadot/api'
 
 export default async (): Promise<ApiPromise> =>  {
 	const wsProvider = new WsProvider("ws://127.0.0.1:9944")
-	const api = ApiPromise.create({provider: wsProvider, types: {}});
-	(await api).isReady
-    return api	
+	return await ApiPromise.create({provider: wsProvider, types: {}});
 }
