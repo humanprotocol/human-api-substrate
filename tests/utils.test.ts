1a5eab5a3d5eb2d46690d033c61d5ec9a910109f
JesseAbram <jesseabramowitz@hotmail.com>
2020-11-27 14:36:30 +0100

    test fix

--- tests/utils.test.ts
+++ tests/utils.test.ts
@@ -6,8 +6,8 @@ const assert = require('assert');
 
 describe('Storage', async () => {
 	it.only("should hash a manifest", async function() {
-		console.log(manifest)
 		const hashed = await hash(JSON.stringify(manifest))
-		console.log(hashed)
+		const result = "0xa750bbd503b5f1a93fcc37b6659455485e638592f57811fcc8e0e96c46f188c4"
+		assert.equal(hashed, result, "hashing should be correct")
 	})
 })
