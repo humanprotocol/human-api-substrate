98efc977b479ef3a598c55dde24c6ce4de68ff0c
JesseAbram <jesseabramowitz@hotmail.com>
2020-12-08 14:43:54 +0100

    allow override of endpoint

--- src/utils/setup.ts
+++ src/utils/setup.ts
@@ -2,8 +2,9 @@ import { ApiPromise, WsProvider, Keyring } from '@polkadot/api'
 import { Setup } from '../interfaces'
 import * as types from '../config/types.json'
 
-export default async (): Promise<Setup> =>  {
-	const wsProvider = new WsProvider("ws://127.0.0.1:9944")
+export default async (endpoint?: string): Promise<Setup> =>  {
+	const url = endpoint ? endpoint : "ws://127.0.0.1:9944"
+	const wsProvider = new WsProvider(url)
 	const api = await ApiPromise.create({provider: wsProvider, types});
 	const keyring = new Keyring({ type: "sr25519" });
 	return {api, keyring}
