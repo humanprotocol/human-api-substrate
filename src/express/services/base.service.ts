708a049885ca5c351bf6a462e886d3316e96f260
JesseAbram <jesseabramowitz@hotmail.com>
2021-01-04 13:06:43 +0100

    added lib functions to express wrapper

--- src/express/services/base.service.ts
+++ src/express/services/base.service.ts
@@ -1,5 +1,3 @@
-import { Keyring } from "@polkadot/api";
-
 import { EscrowId, Job, JobReads, setup } from "../../index";
 
 export const base = async (req: any): Promise<any> => {
@@ -10,6 +8,34 @@ export const base = async (req: any): Promise<any> => {
       return await launch(req.body);
     case "createEscrow":
       return await createEscrow(req.body);
+    case "fundEscrow":
+      return await fundEscrow(req.body);
+    case "addTrustedHandlers":
+      return await addTrustedHandlers(req.body);
+    case "bulkPayout":
+      return await bulkPayout(req.body);
+    case "storeFinalResults":
+      return await storeFinalResults(req.body);
+    case "abort":
+      return await writeNoParams(req.body);
+    case "cancel":
+      return await writeNoParams(req.body);
+    case "noteIntermediateResults":
+      return await noteIntermediateResults(req.body);
+    case "complete":
+      return await writeNoParams(req.body);
+    case "escrow":
+      return await readNoParams(req.body);
+    case "isTrustedHandler":
+      return await isTrustedHandler(req.body);
+    case "balance":
+      return await readNoParams(req.body);
+    case "manifest":
+      return await manifest(req.body);
+    case "intermediateResults":
+      return await intermediateResults(req.body);
+    case "finalResults":
+      return await readNoParams(req.body);
     default:
       throw new Error("invalid function name");
   }
@@ -46,4 +72,82 @@ const createEscrow = async (body: any): Promise<any> => {
   );
 
   return { escrowId: job.escrowId };
+};
+
+const addTrustedHandlers = async (body: any) => {
+  const { escrowId, handlers, seed } = body;
+  const sender = global.keyring.addFromUri(seed);
+  const job = new Job(global.substrate, sender, escrowId);
+
+  await job.addTrustedHandlers(handlers);
+};
+
+const fundEscrow = async (body: any) => {
+  const { amount, escrowAddress, escrowId, seed } = body;
+  const sender = global.keyring.addFromUri(seed);
+  const job = new Job(global.substrate, sender, escrowId);
+
+  await job.fundEscrow(escrowAddress, amount);
+};
+
+const bulkPayout = async (body: any) => {
+  const { escrowId, payouts, seed } = body;
+  const sender = global.keyring.addFromUri(seed);
+  const job = new Job(global.substrate, sender, escrowId);
+
+  await job.bulkPayout(payouts);
+};
+
+const storeFinalResults = async (body: any) => {
+  const { escrowId, results, seed } = body;
+  const sender = global.keyring.addFromUri(seed);
+  const job = new Job(global.substrate, sender, escrowId);
+
+  await job.storeFinalResults(results);
+};
+
+const writeNoParams = async (body: any) => {
+  const { escrowId, functionName, seed } = body;
+  const sender = global.keyring.addFromUri(seed);
+  const job = new Job(global.substrate, sender, escrowId);
+  const functionCall = [functionName] as (keyof typeof job)[];
+
+  await job[`${functionCall}`]();
+};
+
+const noteIntermediateResults = async (body: any) => {
+  const { escrowId, results, seed } = body;
+  const sender = global.keyring.addFromUri(seed);
+  const job = new Job(global.substrate, sender, escrowId);
+
+  await job.noteIntermediateResults(results);
+};
+
+const readNoParams = async (body: any) => {
+  const { escrowId, functionName } = body;
+  const job = new JobReads(global.substrate, escrowId);
+  const functionCall = [functionName] as (keyof typeof job)[];
+
+  await job[`${functionCall}`]();
+};
+
+const isTrustedHandler = async (body: any) => {
+  const { address, escrowId } = body;
+  const job = new JobReads(global.substrate, escrowId);
+
+  await job.isTrustedHandler(address);
+};
+
+const manifest = async (body: any) => {
+  const { escrowId, url } = body;
+  const job = new JobReads(global.substrate, escrowId);
+
+  await job.manifest(url);
+};
+
+const intermediateResults = async (body: any) => {
+  const { escrowId, index } = body;
+  const job = new JobReads(global.substrate, escrowId);
+
+  await job.intermediateResults(index);
 };
--- tests/express.test.ts
+++ tests/express.test.ts
@@ -1,8 +1,31 @@
 import axios from "axios";
 import manifest from "../example-manifest.json";
+const { assert } = require("chai");
 
 describe("express test", async () => {
   const url = "http://localhost:3001";
+  const manifestUrl =
+    "https://human-parity-is-the-best.s3.amazonaws.com/s30x251015a125f7d34f924ac5ac848f120b659f09863e4e355641420f56425833b5";
+  const manifestHash =
+    "0x251015a125f7d34f924ac5ac848f120b659f09863e4e355641420f56425833b5";
+  const reputationOracle = "5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY";
+  const recordingOracle = "5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY";
+  const reputationOracleStake = 5;
+  const recordingOracleStake = 5;
+  let escrowId: any;
+  before(async function () {
+    const returned = await axios.post(url, {
+      functionName: "createEscrow",
+      seed: "//Alice",
+      manifestUrl,
+      manifestHash,
+      reputationOracle,
+      recordingOracle,
+      reputationOracleStake,
+      recordingOracleStake,
+    });
+    escrowId = returned.data.escrowId;
+  });
 
   it(`should launch escrow`, async () => {
     const returned = await axios.post(url, {
@@ -14,15 +37,7 @@ describe("express test", async () => {
     console.log({ escrowId: returned.data.escrowId });
   });
 
-  it.only(`should create an escrow`, async () => {
-    const manifestUrl =
-      "https://human-parity-is-the-best.s3.amazonaws.com/s30x251015a125f7d34f924ac5ac848f120b659f09863e4e355641420f56425833b5";
-    const manifestHash =
-      "0x251015a125f7d34f924ac5ac848f120b659f09863e4e355641420f56425833b5";
-    const reputationOracle = "5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY";
-    const recordingOracle = "5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY";
-    const reputationOracleStake = 5;
-    const recordingOracleStake = 5;
+  it(`should create an escrow`, async () => {
     const returned = await axios.post(url, {
       functionName: "createEscrow",
       seed: "//Alice",
@@ -34,5 +49,41 @@ describe("express test", async () => {
       recordingOracleStake,
     });
     console.log({ escrowId: returned.data.escrowId });
+  });
+  it(`should add a trusted handler`, async () => {
+    const returned = await axios.post(url, {
+      functionName: "addTrustedHandlers",
+      escrowId,
+      seed: "//Alice",
+      handlers: [reputationOracle],
+    });
+    assert.equal(returned.status, "200");
+  });
+  //   it.only(`should do a bulk payout`, async () => {
+
+  // 	const payout = {
+  // 		addresses: [reputationOracle, reputationOracle],
+  // 		amounts: [5, 5],
+  // 	  };
+
+  // 	const returned = await axios.post(url, {
+  // 		functionName: "bulkPayout",
+  // 		escrowId,
+  // 		seed: "//Alice",
+  // 		payouts: payout
+  //   })
+  //   assert.equal(returned.status, "200")
+  // });
+  it.only(`should cancel`, async () => {
+    try {
+      const returned = await axios.post(url, {
+        functionName: "cancel",
+        escrowId,
+        seed: "//Alice",
+      });
+      assert.equal(returned.status, "200");
+    } catch (e) {
+      console.log(e);
+    }
   });
 });
